972f232f87ed515d8f6ce2f1650570966b97c7d4
weijia <richardwangwang@gmail.com>
2026-09-24 22:04:28 +0800

    add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import NoteEditor from './NoteEditor.vue'
+import type { SidebarNote, SaveStatus } from './types'
+
+export { NoteEditor }
+export default NoteEditor
+export * from './types'
+export type { SidebarNote, SaveStatus }
